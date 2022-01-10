25dc4974fbd5769ddaa89915f4cad1233b1c523f
mdaiaesvym <akihirokanaya13@icloud.com>
2022-01-10 18:14:05 +0900

    first commit

---
+++ src/main/java/com/example/demo/controller/Threads.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Threads {
+
+	@GetMapping("/threads")
+	public String getThreads() {
+		return "threads";
+	}
+}
