95ba80bafa5c7184c25fd4d43fc25ab99cfb164c
mdaiaesvym <akihirokanaya13@icloud.com>
2022-01-15 18:30:28 +0900

    クラス名変更

--- src/main/java/com/example/demo/controller/MakeThreadController.java
+++ src/main/java/com/example/demo/controller/MakeThreadController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MakeThread {
+public class MakeThreadController {
 
 	@GetMapping("/makeThread")
 	public String getMakeThread() {
--- src/main/java/com/example/demo/controller/ThreadsController.java
+++ src/main/java/com/example/demo/controller/ThreadsController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Threads {
+public class ThreadsController {
 
 	@GetMapping("/threads")
 	public String getThreads() {
