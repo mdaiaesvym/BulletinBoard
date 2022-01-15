d96ceb9c32cde9877d3b24f7c35bd09d0b25761a
mdaiaesvym <akihirokanaya13@icloud.com>
2022-01-15 23:41:24 +0900

    メッセージ画面追加

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,14 @@
-CREATE TABLE threads
+CREATE TABLE IF NOT EXISTS threads
 (thread_number INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 thread_name VARCHAR(1000) NOT NULL,
 created_at datetime NOT NULL,
 updated_at datetime NOT NULL);
 
-CREATE TABLE messages
+CREATE TABLE IF NOT EXISTS messages
 (thread_number INT NOT NULL,
 message_number INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 message VARCHAR(1000) NOT NULL,
 created_at datetime NOT NULL,
-updated_at datetime NOT NULL),
-FOREIGN KEY(thread_number) REFERENCES threads(thread_number);
+created_name VARCHAR(100) NOT NULL,
+updated_at datetime NOT NULL,
+FOREIGN KEY(thread_number) REFERENCES threads(thread_number));
