d6e8469bfd5cdd41d2e55fe58a909abd5ec66ea3
mdaiaesvym <akihirokanaya13@icloud.com>
2022-02-11 15:00:01 +0900

    初期データの追加

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+/**スレッド名*/
+insert into threads (thread_name,created_at,updated_at)
+values ('雑談','2022-01-01 10:10:10','2022-01-01 10:10:10');
+insert into threads (thread_name,created_at,updated_at)
+values ('今年のベストゲームソフト','2022-01-01 10:10:10','2022-01-01 10:10:10');
+insert into threads (thread_name,created_at,updated_at)
+values ('おすすめの映画','2022-01-01 10:10:10','2022-01-01 10:10:10');
