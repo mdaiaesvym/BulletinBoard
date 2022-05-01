40246b054990c050fce2c474591a62b3cf2ef13a
mdaiaesvym <akihirokanaya13@icloud.com>
2022-05-02 01:07:19 +0900

    変更：フォーマッター実行

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,28 +1,27 @@
 //初期表示
-window.addEventListener('DOMContentLoaded', function(){
-  contributorNameOpenHidden()
+window.addEventListener("DOMContentLoaded", function () {
+  contributorNameOpenHidden();
 });
 
 //投稿者名の表示・非表示
-function contributorNameOpenHidden(){
-
+function contributorNameOpenHidden() {
   var anonymous = document.getElementById("anonymous");
   var contributorNameForm = document.getElementById("contributorNameForm");
   var contributorNameTextArea = document.getElementById("contributorName");
 
-  if(anonymous.checked){
+  if (anonymous.checked) {
     contributorNameForm.style.display = "none";
     contributorNameTextArea.value = "";
-  }else{
+  } else {
     contributorNameForm.style.display = "";
   }
 }
 
 //匿名で投稿の場合、投稿者名に「匿名」を入れる
-function setAnonymous(){
+function setAnonymous() {
   var checkContributorName = document.querySelector("input[name=checkContributorName]:checked");
 
-  if(checkContributorName.value==="0"){
-    document.getElementById("contributorName").value="匿名"
+  if (checkContributorName.value === "0") {
+    document.getElementById("contributorName").value = "匿名";
   }
 }
--- src/main/resources/static/js/messages.js
+++ src/main/resources/static/js/messages.js
@@ -1,9 +1,9 @@
-function replay(count){
-  messageArea = document.getElementById("message")
+function replay(count) {
+  messageArea = document.getElementById("message");
 
   //対象のメッセージ番号をメッセージのテキストエリアに入れる
-  messageArea.value=">>" + count + "\n"
+  messageArea.value = ">>" + count + "\n";
 
   //マウスカーソルをメッセージに合わせる
-  messageArea.focus()
+  messageArea.focus();
 }
