ab38b36531d6c5ac7c55a75d58ecafe60cded12a
mdaiaesvym <akihirokanaya13@icloud.com>
2022-05-01 22:12:41 +0900

    追加：バリデーション

--- src/main/java/com/example/demo/form/MakeMessageForm.java
+++ src/main/java/com/example/demo/form/MakeMessageForm.java
@@ -2,6 +2,8 @@ package com.example.demo.form;
 
 import javax.validation.constraints.NotBlank;
 
+import org.hibernate.validator.constraints.Length;
+
 import lombok.Data;
 
 @Data
@@ -9,8 +11,10 @@ public class MakeMessageForm {
 
 	private Integer threadNumber;
 	@NotBlank
+	@Length(min = 1, max = 1000)
 	private String message;
 	private Integer checkContributorName = 0;
 	@NotBlank
+	@Length(min = 1, max = 100)
 	private String contributorName;
 }
