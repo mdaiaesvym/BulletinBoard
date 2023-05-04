c80a89600e4c278d38f0c266368b7eb2a50b3be3
mdaiaesvym <akihirokanaya13@icloud.com>
2023-05-04 20:43:41 +0900

    変更：イベント発火方法

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,13 +1,16 @@
 //初期表示
 window.addEventListener("DOMContentLoaded", function () {
-  contributorNameOpenHidden();
+  const anonymous = document.querySelector("#anonymous");
+  anonymous.addEventListener("change", () => contributorNameOpenHidden());
+  const onymous = document.querySelector("#onymous");
+  onymous.addEventListener("change", () => contributorNameOpenHidden());
 });
 
 //投稿者名の表示・非表示
 function contributorNameOpenHidden() {
-  var anonymous = document.getElementById("anonymous");
-  var contributorNameForm = document.getElementById("contributorNameForm");
-  var contributorNameTextArea = document.getElementById("contributorName");
+  const anonymous = document.querySelector("#anonymous");
+  const contributorNameForm = document.querySelector("#contributorNameForm");
+  const contributorNameTextArea = document.querySelector("#contributorName");
 
   if (anonymous.checked) {
     contributorNameForm.setAttribute("hidden", "");
