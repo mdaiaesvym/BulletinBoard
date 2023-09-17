330df89ab2924f0a98d754993cf5fa784dff9a0f
mdaiaesvym <akihirokanaya13@icloud.com>
2023-09-17 20:44:30 +0900

    変更（html,java）：フォーマットをjava側で実装

--- src/main/java/com/example/demo/model/Message.java
+++ src/main/java/com/example/demo/model/Message.java
@@ -1,5 +1,6 @@
 package com.example.demo.model;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import lombok.Data;
 
@@ -10,8 +11,14 @@ public class Message {
   private Integer threadNumber;
   // メッセージ
   private String message;
-  // 更新日時
+  // 更新日時（フォーマット：yyyyMMddhhmmss）
   private Date updatedAt;
   // 投稿者名
   private String contributorName;
+
+  // yyyyMMddhhmmss→yyyy/MM/dd hh:mm:ssに変換
+  public String getFormatUpdatedAt() {
+    SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    return outputFormat.format(this.updatedAt);
+  }
 }
--- src/main/java/com/example/demo/model/Thread.java
+++ src/main/java/com/example/demo/model/Thread.java
@@ -1,5 +1,6 @@
 package com.example.demo.model;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import lombok.Data;
 
@@ -10,8 +11,14 @@ public class Thread {
   private Integer threadNumber;
   // スレッド名
   private String threadName;
-  // 更新日時
+  // 更新日時（フォーマット：yyyyMMddhhmmss）
   private Date updatedAt;
   // コメント数
   private Integer messageCount;
+
+  // yyyyMMddhhmmss→yyyy/MM/dd hh:mm:ssに変換
+  public String getFormatUpdatedAt() {
+    SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    return outputFormat.format(this.updatedAt);
+  }
 }
