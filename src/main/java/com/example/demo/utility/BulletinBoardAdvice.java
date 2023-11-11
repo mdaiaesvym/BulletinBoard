76ec56e627b47d847da15442365c892a029b6c57
mdaiaesvym <akihirokanaya13@icloud.com>
2023-11-11 22:59:56 +0900

    変更（java）：コメント

--- src/main/java/com/example/demo/utility/BulletinBoardAdvice.java
+++ src/main/java/com/example/demo/utility/BulletinBoardAdvice.java
@@ -5,11 +5,16 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.InitBinder;
 
 /**
- * アプリケーション内のすべてのコントローラーに対する事前処理
+ * アプリケーション内のすべてのコントローラーに対する共通処理
  */
 @ControllerAdvice
 public class BulletinBoardAdvice {
 
+  /**
+   * 各Controllerが呼び出される前の事前処理
+   * 
+   * @param binder
+   */
   @InitBinder
   public void initBinder(WebDataBinder binder) {
     binder.registerCustomEditor(String.class, new CustomStringTrimmer());
