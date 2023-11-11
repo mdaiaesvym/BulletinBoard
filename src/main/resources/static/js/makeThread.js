a3db4147df765f9bb7ee342f1c94c7520ebb4b9a
mdaiaesvym <akihirokanaya13@icloud.com>
2023-11-11 20:40:56 +0900

    変更（js）：投稿者名の表示非表示処理の実装方法を変更

--- src/main/resources/static/js/makeThread.js
+++ src/main/resources/static/js/makeThread.js
@@ -1,3 +1,11 @@
+//初期実行
+window.addEventListener("load", function () {
+  document.querySelectorAll(".contributorName").forEach((nameArea) => {
+    nameArea.addEventListener("change", () => switchingContributorNameArea());
+  });
+});
+
+//作成ボタンをクリック
 document.querySelector("#makeThreadButton").addEventListener("click", () => {
   //メッセージ取得
   const message = document.querySelector("#postMessage").value;
@@ -7,3 +15,17 @@ document.querySelector("#makeThreadButton").addEventListener("click", () => {
     document.querySelector("#makeThreadForm").submit();
   }
 });
+
+//投稿者名の表示・非表示
+function switchingContributorNameArea() {
+  const anonymous = document.querySelector("#anonymous");
+  const contributorNameForm = document.querySelector("#contributorNameForm");
+  const contributorNameTextArea = document.querySelector("#contributorName");
+
+  if (anonymous.checked) {
+    contributorNameForm.setAttribute("hidden", "");
+    contributorNameTextArea.value = "";
+  } else {
+    contributorNameForm.removeAttribute("hidden");
+  }
+}
--- src/main/resources/static/js/messages.js
+++ src/main/resources/static/js/messages.js
@@ -1,14 +1,11 @@
-//メッセージ入力フォームに返信先の番号を入れ、メッセージ入力フォームにフォーカスする
-function replay(count) {
-  messageArea = document.querySelector("#message");
+//初期実行
+window.addEventListener("load", function () {
+  document.querySelectorAll(".contributorName").forEach((nameArea) => {
+    nameArea.addEventListener("change", () => switchingContributorNameArea());
+  });
+});
 
-  //対象のメッセージ番号をメッセージのテキストエリアに入れる
-  messageArea.value = ">>" + count + "\n";
-
-  //マウスカーソルをメッセージに合わせる
-  messageArea.focus();
-}
-
+//投稿ボタンをクリック
 document.querySelector("#makeMessageButton").addEventListener("click", () => {
   //メッセージ取得
   const message = document.querySelector("#postMessage").value;
@@ -18,3 +15,29 @@ document.querySelector("#makeMessageButton").addEventListener("click", () => {
     document.querySelector("#makeMessageForm").submit();
   }
 });
+
+//投稿者名の表示・非表示
+function switchingContributorNameArea() {
+  const anonymous = document.querySelector("#anonymous");
+  const contributorNameForm = document.querySelector("#contributorNameForm");
+  const contributorNameTextArea = document.querySelector("#contributorName");
+
+  if (anonymous.checked) {
+    contributorNameForm.setAttribute("hidden", "");
+    contributorNameTextArea.value = "";
+  } else {
+    contributorNameForm.removeAttribute("hidden");
+  }
+}
+
+//メッセージ入力フォームに返信先の番号を入れ、メッセージ入力フォームにフォーカスする
+//htmlから呼び出している
+function replay(count) {
+  messageArea = document.querySelector("#message");
+
+  //対象のメッセージ番号をメッセージのテキストエリアに入れる
+  messageArea.value = ">>" + count + "\n";
+
+  //マウスカーソルをメッセージに合わせる
+  messageArea.focus();
+}
