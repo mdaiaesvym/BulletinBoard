18df347071836c9e5fbd2d9129f3e872a1b4bbd0
mdaiaesvym <akihirokanaya13@icloud.com>
2023-11-23 19:24:31 +0900

    変更：ファイル名

--- src/test/java/mapper/BulletinBoardMapperTest.java
+++ src/test/java/mapper/BulletinBoardMapperTest.java
@@ -14,7 +14,7 @@ import com.example.demo.repository.BulletinBoardMapper;
 
 @MybatisTest
 @ContextConfiguration(classes = BulletinBoardApplication.class)
-public class BulletinBoardTest {
+public class BulletinBoardMapperTest {
 
   @Autowired
   private BulletinBoardMapper mapper;
